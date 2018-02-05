cf282610458270bf1756760a1a5fcc4f3641910b
curs2018 <curs2018@BI101-15.epsevg.upc.es>
2018-02-05 12:57:52 +0100

    js5

--- JAVASCRIPT/E5/js/codigo.js
+++ JAVASCRIPT/E5/js/codigo.js
@@ -1,8 +1,8 @@
 var numero1 = 5;
 var numero2 = 8;
 
-if (numero1>numero2) {
-    alert("Numero no es mayour que numero2");
+if (numero1<=numero2) {
+    alert("Numero1 no es mayor que numero2");
 }
 
 if (numero2 > 0) {
@@ -13,8 +13,8 @@ if (numero1 < 0 || numero1 != 0) {
     alert("Numero1 es negativo o distinto de cero");
 }
 
-if (++numero) {
-    alert("Incrementar en 1 unidad el valor de numero1 no lo hace mayour o igual que numero2");
+if (++numero1 <= numero2) {
+    alert("Incrementar en 1 unidad el valor de numero1 no lo hace mayor o igual que numero2");
 }
 
 
