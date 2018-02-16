b33596e214a1a4d35eb3390b6098d8e5833190a7
curs2018 <curs2018@BI101-15.epsevg.upc.es>
2018-02-16 13:39:58 +0100

    updates

---
+++ JAVASCRIPT/EjerciciosApuntes/E3/js/euroPeseta.js
@@ -0,0 +1,39 @@
+window.addEventListener("load", cargaEventos);
+
+function cargaEventos() {
+    document.getElementById("GetEuros").addEventListener("click", calcularEuros);
+    document.getElementById("GetPesetas").addEventListener("click", calcularPesetas);
+}
+
+function calcularEuros() {
+    var pesetas = document.getElementById("pesetas").value;
+    if (pesetas > 0 && pesetas % 1 == 0) {
+        Euros= pesetas / 166.386;
+        Euros = Euros.toFixed(2);
+        document.getElementById("resultEuros").innerHTML = pesetas + " pesetas = " + Euros + " euros" ;
+        document.getElementById("pesetas").value ="";  
+    } else {
+        alert("El valor tiene que ser un número entero y mayor que zero!");
+        document.getElementById("pesetas").value = "";  
+          
+    }      
+}
+
+function calcularPesetas() {
+    var euros = document.getElementById("euros").value;
+    euros = parseFloat(euros).toFixed(2);
+    console.log(euros);
+    console.log(parseFloat(euros).toFixed(2));    
+    if (euros > 0 ) {
+        Pesetas = euros * 166.386;
+        Pesetas = Pesetas.toFixed();
+        document.getElementById("resultPesetas").innerHTML = euros + " euros = " + Pesetas + " pesetas";
+        document.getElementById("euros").value = "";
+    } else {
+        alert("El valor tiene que ser númerico y mayor que zero!");
+        document.getElementById("euros").value = "";  
+    }
+
+    
+}
+
