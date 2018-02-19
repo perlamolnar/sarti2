6bc20ce9603a725725c30f801d391b819c48e5fa
curs2018 <curs2018@BI101-15.epsevg.upc.es>
2018-02-19 14:01:04 +0100

    updates

---
+++ JAVASCRIPT/EjerciciosApuntes/visorImagenes1/js/visorImagenes.js
@@ -0,0 +1,22 @@
+window.addEventListener("load",getReady);
+
+function getReady(){
+    /* Cargar todos los eventos de la página */ 
+    var imagenes = document.querySelectorAll("#smallImg img");
+    for (foto of imagenes) {
+        console.log(foto);
+        foto.addEventListener("click", visor);        
+    }
+}
+       
+function visor() {
+    var bigImg = document.getElementById("bigImg");
+    var imgText = document.getElementById("imgText");
+    
+    bigImg.src = this.src; 
+    imgText.innerHTML = this.alt;
+
+    document.getElementById("bigImg").innerHTML=bigImg;   
+    //bigImg.parentElement.style.display = "block";  
+}
+
