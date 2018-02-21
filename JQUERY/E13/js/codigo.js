fa6bd99286ee37f288abf016525950e8312cc4ef
curs2018 <curs2018@BI101-15.epsevg.upc.es>
2018-02-21 13:59:11 +0100

    updates

--- JQUERY/E13/js/codigo.js
+++ JQUERY/E13/js/codigo.js
@@ -1,42 +1,43 @@
-window.addEventListener("load", cargaEventos);
+$(document).ready(function () {
+    $("#addElement").on("click", add);
+    $("#deleteElement").on("click", borrar);
+    
+});
 
-function cargaEventos() {
-    document.getElementById("addElement").addEventListener("click", add);
-    document.getElementById("deleteElement").addEventListener("click", borrar);
+function add() {
+    var newElement = $("#NewElement").value;
+    $("#lista").text = $("#lista").text + "<li>" + newElement + "</li>";
+    $("#NewElement").value = (" ");
+}
+
+
+function borrar() {
     
 }
 
+//otra version:
 // function add() {
-//     var newElement = document.getElementById("NewElement").value; //recojer texto de input    
-//     document.getElementById("lista").innerHTML = document.getElementById("lista").innerHTML + "<li>"+newElement+"</li>";
-//     document.getElementById("NewElement").value=" ";
+//     var newElement = document.getElementById("NewElement").value;
+//     var array_li = document.getElementsByTagName("li");
+//     if (array_li.length ==0){
+//         document.getElementById("deleteElement").disabled = false;
+//     }
+//     var elemento = document.createElement("li");
+//     var texto =document.createTextNode(newElement);
+//     elemento.appendChild(texto); //añadimos el texto en el elemento
+//     var lista = document.getElementById("lista");
+//     lista.appendChild(elemento);
+//     document.getElementById("NewElement").value = " ";
 // }
 
+// function borrar() {
+//     //var lista = document. getElementById("lista");
+//     var array_li = lista.getElementsByTagName("li");
 
+//     var ultimo_li = array_li[array_li.length-1];
+//     ultimo_li.parentNode.removeChild(ultimo_li); //borramos el ultimo elemento
 
-//otra version:
-function add() {
-    var newElement = document.getElementById("NewElement").value;
-    var array_li = document.getElementsByTagName("li");
-    if (array_li.length ==0){
-        document.getElementById("deleteElement").disabled = false;
-    }
-    var elemento = document.createElement("li");
-    var texto =document.createTextNode(newElement);
-    elemento.appendChild(texto); //añadimos el texto en el elemento
-    var lista = document.getElementById("lista");
-    lista.appendChild(elemento);
-    document.getElementById("NewElement").value = " ";
-}
-
-function borrar() {
-    //var lista = document. getElementById("lista");
-    var array_li = lista.getElementsByTagName("li");
-
-    var ultimo_li = array_li[array_li.length-1];
-    ultimo_li.parentNode.removeChild(ultimo_li); //borramos el ultimo elemento
-
-    if (array_li.length==0) {
-        document.getElementById("deleteElement").disabled = true;        
-    }
-}
+//     if (array_li.length==0) {
+//         document.getElementById("deleteElement").disabled = true;        
+//     }
+// }
