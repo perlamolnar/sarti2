5fedc37f0c273d505d0172caddcf5dcf9082b2c8
curs2018 <curs2018@BI101-15.epsevg.upc.es>
2018-02-21 13:43:26 +0100

    updates

--- JQUERY/E12/js/codigoB.js
+++ JQUERY/E12/js/codigoB.js
@@ -1,13 +1,13 @@
 //CON 1 ENLACE SOLO!!!
 $(document).ready(function () {
     $("#adicional").hide(); 
+    
     $("#enlace1").click(function () {
         $("#adicional").toggle().css("color", "red");
         
         var textBoton = $("#adicional").css("display");
         if (textBoton == "none") {
             $("#enlace1").text("Leer mas");
-
             
         } else {
             $("#enlace1").text("Leer menos");
