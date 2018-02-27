1adb93fa172e85bc4752cec261e278ea378d3a42
curs2018 <curs2018@BI101-15.epsevg.upc.es>
2018-02-27 13:59:06 +0100

    updates

--- Jquery UI/E4/js/E4.js
+++ Jquery UI/E4/js/E4.js
@@ -1,30 +1,35 @@
 $(document).ready(cargaEventos);
 
 function cargaEventos() { 
-  $("#datepicker").datepicker();
-    
-  $("#datepicker1").datepicker();
-     $("#anim").on("change", function () {
-      $("#datepicker1").datepicker("option", "showAnim", $(this).val());
-  });
-
-  $("#datepicker2").datepicker();
-   $("#format").on("change", function () {
-     $("#datepicker2").datepicker("option", "dateFormat","dd-mm-yy");
-       });
   
-  $("#datepicker3").datepicker({ minDate: -20, maxDate: "+1M +10D" });
-
-  $("#datepicker4").datepicker({
-    showAnim: "slideDown",
-    dateFormat: "yy-mm-dd",
-    minDate: 0, //desde hoy
-    maxDate: "+1M +10D"
-
-  });
 
   
 }
 
 
+// function cargaEventos() {
+//   $("#datepicker").datepicker();
+
+//   $("#datepicker1").datepicker();
+//   $("#anim").on("change", function () {
+//     $("#datepicker1").datepicker("option", "showAnim", $(this).val());
+//   });
+
+//   $("#datepicker2").datepicker();
+//   $("#format").on("change", function () {
+//     $("#datepicker2").datepicker("option", "dateFormat", "dd-mm-yy");
+//   });
+
+//   $("#datepicker3").datepicker({ minDate: -20, maxDate: "+1M +10D" });
+
+//   $("#datepicker4").datepicker({
+//     showAnim: "slideDown",
+//     dateFormat: "yy-mm-dd",
+//     minDate: 0, //desde hoy
+//     maxDate: "+1M +10D"
+
+//   });
+
+
+// }
 
