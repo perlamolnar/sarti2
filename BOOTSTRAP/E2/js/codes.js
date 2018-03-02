4ef7ae3b60176f7dd3bf12a15b623e2f8d36a607
curs2018 <curs2018@BI101-15.epsevg.upc.es>
2018-03-02 12:47:23 +0100

    updates

---
+++ BOOTSTRAP/E2/js/codes.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+    $("#aviso").hide();
+    $("#enviar").on("click", aviso);
+    $("#bclosewarn").on("click", function(){
+
+    });
+
+    $('#aviso').on('close.bs.alert', function () {
+        $("#aviso").hide();
+
+        return false;
+    })
+
+});
+
+function aviso() {
+    var usuario = $("#username").val();
+    var password = $("#password").val();
+    console.log(usuario);
+    console.log(password);
+
+    if (usuario == "" || password =="") {        
+        $("#aviso").show();
+
+    } else {
+        $('form').submit();        
+    }
+    console.log(usuario);
+    console.log(password);
+}
+
+
+// $(document).ready(function () {
+
+//     $("#enviar").on("click", aviso);
+
+//     $("#aviso").hide();
+//     $('#aviso').on('close.bs.alert', function () {
+        //hemos eliminado en el html el "dissmiss alert y data-dismiss"
+//         $(this).hide();
+//         return false;
+//     })
+
+// });
+
+// function aviso() {
+//     var usuario = $("#username").val();
+//     var password = $("#password").val();
+//     console.log(usuario);
+//     console.log(password);
+
+//     if (usuario == "" || password == "") {
+//         $("#aviso").show();
+
+//     } else {
+//         $('form').submit();
+//     }
+//     console.log(usuario);
+//     console.log(password);
+// }
