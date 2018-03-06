857355fca9114d398127d6233290dffe7619f0a3
curs2018 <curs2018@BI101-15.epsevg.upc.es>
2018-03-06 13:02:50 +0100

    updates

--- BOOTSTRAP/EP/js/codes.js
+++ BOOTSTRAP/EP/js/codes.js
@@ -1,3 +1,18 @@
+document.querySelector(".card-flip").classList.toggle("flip");
+
+/* 
+ * Holder.js for demo image
+ * Just for demo purpose
+ */
+Holder.addTheme('gray', {
+    bg: '#777',
+    fg: 'rgba(255,255,255,.75)',
+    font: 'Helvetica',
+    fontweight: 'normal'
+});
+
+
+
 $(document).ready(function () {
     //LOGIN  
     $("#enviar").on("click", aviso);
