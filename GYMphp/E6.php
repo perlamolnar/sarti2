0f1ba7d1d4ff507d62fed18374c6791ca85437bf
curs2018 <curs2018@BI101-15.epsevg.upc.es>
2018-03-22 09:44:30 +0100

    updates

--- GYMphp/E6.php
+++ GYMphp/E6.php
@@ -1,3 +1,35 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+    <style>
+    table {}
+    </style>
+</head>
+<body>
+	<table border="1">
+		
+		<?php
 
-?>
+			for ($i=1; $i < 11; $i++) { 
+                print("<tr> ");
+
+                    for ($x=1; $x <11 ; $x++) { 
+                        print("<th>$i</th> 
+                    ");
+                    }
+                    
+                print("</tr> ");
+            }
+            
+
+		?>
+
+
+
+
+
+
+	</table>
+</body>
+</html>
