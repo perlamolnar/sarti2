789e9e5991eb6f187a2c8d82740d55a94922d5c6
curs2018 <curs2018@BI101-15.epsevg.upc.es>
2018-03-22 10:22:36 +0100

    updates

--- GYMphp/E6.php
+++ GYMphp/E6.php
@@ -3,33 +3,69 @@
 <head>
 	<title></title>
     <style>
-    table {}
+        body{background:}
+        .orange{background-color: darkorange;}
+        .blue{background-color: lightblue;}
+
+        
     </style>
 </head>
 <body>
+<h1>TABLA1 DE MULTIPLICACIÓN</h1>
 	<table border="1">
 		
 		<?php
 
-			for ($i=1; $i < 11; $i++) { 
-                print("<tr> ");
+            //$head="<tr><th></th></tr>"
+
+			for ($i=1; $i < 11; $i++) {               
+                
+                print("<tr> ");                
 
                     for ($x=1; $x <11 ; $x++) { 
-                        print("<th>$i</th> 
-                    ");
+                        $resultado=$i*$x;
+                        echo("<td>$resultado</td>");
                     }
-                    
+
                 print("</tr> ");
             }
             
-
 		?>
 
+	</table>
 
+<h1>TABLA2 DE MULTIPLICACIÓN</h1>
+    <table border="1">
+		
+		<?php   
+			for ($i=1; $i < 11; $i++) { 
+                if ($i%2==0) {
+                    $clasp="orange";
+                } else {
+                    $clasp="blue";
+                }
+                 
+        ?> 
 
+        <tr class="<?= $clasp?>">
 
+        <?php for ($z=1; $z < 11; $z++) { ?> 
+            
+            <td><?= $i*$z?></td>
 
+         <?php   } ?>
+       
+        
+        </tr>        
+        <?php   } ?>        
+               
 
 	</table>
+
+
+
+
+
+
 </body>
 </html>
