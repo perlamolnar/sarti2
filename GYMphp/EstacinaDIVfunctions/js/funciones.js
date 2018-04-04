de3d83ff5417c16bbaecd0bfecdb988f1b1f1f2e
curs2018 <curs2018@BI101-15.epsevg.upc.es>
2018-04-04 13:54:58 +0200

    updates

---
+++ GYMphp/EstacinaDIVfunctions/js/funciones.js
@@ -0,0 +1,22 @@
+function cargarContenido(receta, estacion_) {  //desde el cuerpo, atraves del onclick cogemos los parametros ('$nombreFichero', '$estacion') y los metemos en la funcion
+    
+    $.post("receta.php", {nomArchivo:receta, estacion:estacion_}, function(data){ 
+
+        $("#mainBox").html(data);
+    });
+    
+}
+
+
+//Change the text of a <div> element using an AJAX POST request:
+
+    //     $.post("demo_ajax_gethint.asp", {suggest: txt}, function(result){
+    //         $("span").html(result);
+    //     });
+
+    // Sintax:
+    // $.post(URL,data,callback);
+    //     1. parameter of $.post() is the URL we wish to request ("receta.php")
+    //     2. Then we pass in some data to send along with the request (receta y estacion).
+    //         The <?php script in "receta.php" reads the parameters, processes them, and returns a result.
+    //     3. parameter is a callback function. The first callback parameter holds the content of the page requested, and the second callback parameter holds the status of the request.
--- GYMphp/calculadora/calculadora.js
+++ GYMphp/calculadora/calculadora.js
@@ -8,9 +8,17 @@ function calcular() {
     var number2 = $("#numero2").val();
     var operacion = $("#operacion").val();
     
-    $.post("calculadora.php", { number1: number1, number2: number2, operacion: operacion}, function (data) {
+    if(number1=="" ||number2==""){
+        $("#resultado").html("En algún lugar no has puesto ningún número.");
+        
+    }
+    else{
+        $.post("calculadora.php", { number1: number1, number2: number2, operacion: operacion}, function (data) {
 
         $("#resultado").html(data);
     });
+    }
+    
+    
     
 }
