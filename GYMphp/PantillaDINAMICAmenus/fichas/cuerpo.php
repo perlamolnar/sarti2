63f21954bcc098d4ff93d3a5a0bf8c8eac2dd095
curs2018 <curs2018@BI101-15.epsevg.upc.es>
2018-04-12 09:17:41 +0200

    updates

---
+++ GYMphp/PantillaDINAMICAmenus/fichas/cuerpo.php
@@ -0,0 +1,49 @@
+<div id="cuerpo">
+    
+    <div id="menu">
+        <h2>MENU</h2>
+            <?php
+                $directorio = 'fichas/RecetasTEXTO/'.$estacion; //damos el nombre de la carpeta donde estan los archivos
+                $ficheros  = scandir($directorio); //scaneamos la carpeta para leer sus archivos/ficheros y los guardamos en un array
+                foreach ($ficheros as $nombreFichero) {    //recorremos el array para ver sus elementos: expresión_array as $valor       
+                    
+                    if ($nombreFichero!="." && $nombreFichero!="..") {           
+                        //echo "<a href=\"receta.php?nomArchivo=$nombreFichero&estacion=$estacion\">$nombreFichero</a><br>";  //ir y pintar receta a otra pagina
+                        echo "<a href=\"#\" onclick=\"cargarContenido('$nombreFichero', '$estacion')\";>".substr($nombreFichero, 0, -4)."</a><br>"; //pintar receta en la misma pagina en un div
+                        //usamos la substr() function para quitar el .txt al final de nombre de fichero
+
+                        echo "<a href=\"#\" onclick=\"cargarContenido('$nombreFichero', '$estacion')\";><img src=\"img/".substr($nombreFichero, 0, -4).".jpg\"></a><br>";
+
+                        //echo "<img src=\"img/$nombreFichero.jpg\">";  
+                    }
+                  
+                } 
+                
+            ?>
+
+    </div>
+
+    <div id="mainBox">
+        <img src="img/cocinera.png" alt="Cocinera">
+        <?php               
+            
+                // $directorio = 'fichas/RecetasTEXTO/'.$estacion; //damos el nombre de la carpeta donde estan los archivos text
+                // $ficheros  = scandir($directorio);               
+
+                // foreach ($ficheros as $nombreFichero) {           
+                     
+                //     if ($nombreFichero!="." && $nombreFichero!="..") {          
+                //         //echo utf8_encode($line)."<br>";
+                //         //echo $nombreFichero."<br>";                       
+
+                //         $newRececta=file($directorio."/".$nombreFichero); //en la file()function ponemos la ruta al fichero
+                                                
+                //         foreach($newRececta as $line){                            
+                //         //echo (<img src="img/invierno1.jpg">);                        
+                //         //echo ($line)."<br>";
+                //         } 
+                //     }
+                // } 
+        ?>
+    </div>	
+</div>
