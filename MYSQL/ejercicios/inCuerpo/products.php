343db6a6bc7d199abc0029e377ffca30cf93f97f
curs2018 <curs2018@BI101-15.epsevg.upc.es>
2018-05-22 13:59:03 +0200

    update

--- MYSQL/ejercicios/inCuerpo/products.php
+++ MYSQL/ejercicios/inCuerpo/products.php
@@ -52,7 +52,7 @@
                         echo "<td><img src=\"img/$Foto\"></td>"; 
                         
                         echo    "<td>
-                                    <button onclick=\"editar($ID);\" class=\"btn\"><i class=\"fa fa-edit\"></i></button>
+                                    <button onclick=\"editar($ID);\" class=\"btn \"><i class=\"fa fa-edit\"></i></button>
                                 </td>"; 
                         echo    "<td>
                                     <button onclick=\"borrar($ID);\" class=\"btn\"><i class=\"fa fa-close\"></i></button>
@@ -64,13 +64,48 @@
                 
             </tbody>
         </table>
-    </div>
-    
+    </div>   
 
     <?php
     mysqli_close($conexion);
     ?>
 
+
+
+
+<div class="container">
+  
+  <!-- Button to Open the Modal -->
+  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+    Open modal
+  </button>
+
+  <!-- The Modal -->
+  <div class="modal fade" id="myModal">
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+      
+        <!-- Modal Header -->
+        <div class="modal-header">
+          <h4 class="modal-title">Modal Heading</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        
+        <!-- Modal body -->
+        <div class="modal-body">
+          Modal body..
+        </div>
+        
+        <!-- Modal footer -->
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+  
+</div>
 </body>
 </html>
 
