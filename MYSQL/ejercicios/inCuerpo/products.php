2596bd95c12f4d8d39ddaae0cfa639e6de42fedc
curs2018 <curs2018@BI101-15.epsevg.upc.es>
2018-05-23 11:23:45 +0200

    update

--- MYSQL/ejercicios/inCuerpo/products.php
+++ MYSQL/ejercicios/inCuerpo/products.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="js/funciones.js"></script>
-    
+    <!-- <script src="js/funciones.js"></script>     -->
     <title>Document</title>
 </head>
 <body>
@@ -52,8 +51,9 @@
                         echo "<td><img src=\"img/$Foto\"></td>"; 
                         
                         echo    "<td>
-                                    <button onclick=\"editar($ID);\" class=\"btn \"><i class=\"fa fa-edit\"></i></button>
+                                    <button onclick=\"Modal1('$ID',  '$Nombre', '$Descripcion', '$Precio', '$Foto');\" class=\"btn\"><i class=\"fa fa-edit\"></i></button>
                                 </td>"; 
+
                         echo    "<td>
                                     <button onclick=\"borrar($ID);\" class=\"btn\"><i class=\"fa fa-close\"></i></button>
                                 </td>";                        
@@ -69,43 +69,140 @@
     <?php
     mysqli_close($conexion);
     ?>
-
-
-
-
-<div class="container">
   
-  <!-- Button to Open the Modal -->
-  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
-    Open modal
-  </button>
 
-  <!-- The Modal -->
-  <div class="modal fade" id="myModal">
+  <!-- Modal -->
+  <div class="modal fade" id="myModal" role="dialog">
     <div class="modal-dialog modal-lg">
       <div class="modal-content">
-      
-        <!-- Modal Header -->
         <div class="modal-header">
-          <h4 class="modal-title">Modal Heading</h4>
           <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">MODIFICAR PRODUCTO</h4>
         </div>
-        
-        <!-- Modal body -->
-        <div class="modal-body">
-          Modal body..
+        <div class="modal-body ">
+          	<form id="formProducto" enctype="multipart/form-data">
+				<div class="container">
+
+					<input type="hidden" name="Id_producto" id="Id_producto" >
+
+					<div class="row">
+						<div class="active col s12 m12 l12 input-field">
+                            Nombre:<br>
+                            <input id="Nombre" name="Nombre" type="text" class="validate">
+						</div>				
+		
+						<div class="col s12 m12 l12 input-field">
+							Descripción:<br>
+							<input type="text" id="Descripcion" name="Descripcion"</input>
+						</div>	
+
+                        <div class="col s12 m12 l12 input-field">
+							Precio:<br>
+							<input type="number" id="Precio" name="Precio"</input>
+						</div>					
+		
+						<div class="col s12 m12 l12">
+							<img id="Foto" name="Foto" scr="" width="20%" >
+						</div>
+						
+						<!-- materialize "input field" codes -->
+						<div class="col s12 m12 l12 file-field input-field">
+							<div class="btn">
+							Subir Imagen: <br>
+							<input type="file" name="Foto">
+							</div>
+							<div class="file-path-wrapper">
+							<input class="file-path validate" id="Foto" name="Foto" type="text">
+							</div>
+						</div>
+					</div>
+				</div>							
+			</form>
         </div>
-        
-        <!-- Modal footer -->
         <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" onclick="modificarProduco();" class="btn btn-primary">GUARDAR</button>
+          <button type="button" class="btn btn-default" data-dismiss="modal">CERRAR</button>
         </div>
-        
       </div>
     </div>
   </div>
-  
+
+
+
+
+<div id="VerModificarArticulo" class="modal">
+		<div class="modal-content">
+		<h3>MODIFICAR ARTICULO</h3>
+	
+			<form id="formArticulo" enctype="multipart/form-data">
+				<div class="">
+
+					<input type="hidden" name="idArticulo" id="idArticulo1">
+
+					<div class="row">
+						<div class="active col s12 m12 l12 input-field">
+						Titulo:
+						<input id="Titulo1" name="Titulo" type="text" class="validate">
+										
+						</div>				
+		
+						<div class="col s12 m12 l12 input-field">
+							Descripción:
+							<textarea id="Descripcion1" name="Descripcion" class="materialize-textarea"></textarea>
+							
+						</div>
+
+						<div class="col s12 m12 l12 input-field">
+							Articulo:
+							<textarea id="Articulo1" name="Articulo" class="materialize-textarea"></textarea>
+						</div>
+		
+						<div class="col s12 m12 l12">
+							<img id="Foto2" name="Foto2" scr="" width="20%" >
+						</div>
+						
+						<!-- materialize "input field" codes -->
+						<div class="col s12 m12 l12 file-field input-field">
+							<div class="btn">
+							<span>Subir Imagen</span>
+							<input type="file" name="Foto">
+							</div>
+							<div class="file-path-wrapper">
+							<input class="file-path validate" id="Foto1" name="Foto1" type="text">
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="row align center">
+					<div class="col s12 m4 l4 input-field">
+					<button class="btn waves-effect waves-light" onclick="modificarArticulo();" name="action">GUARDAR			
+					</button>
+					</div>
+	
+					<div class="col s12 m4 l4 input-field">
+					<button class="btn waves-effect waves-light red" onclick="removeArticulo();" name="action">ELIMINAR		
+					</button>
+					</div>
+	
+					<div class="col s12 m4 l4 input-field">
+					<button class="btn waves-effect waves-light modal-close grey">CERRAR		
+					</button>				
+					</div>
+				</div>				
+			</form>     
+		</div>					
+	</div>
+	<div class="modal-footer" style="background-color: #8b6f47">
+	  <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat ">Cerrar</a>
+	</div>
 </div>
+
+
+
+
+  
+
 </body>
 </html>
 
--- MYSQL/ejercicios/js/funciones.js
+++ MYSQL/ejercicios/js/funciones.js
@@ -1,3 +1,71 @@
+function borrar(Id) {
+    //console.log(Id);
+    $.ajax({
+        type: 'POST',
+        url: 'php/deleteproducto.php',
+        //dataType: "json",
+        data: { id: Id },
+        success: function (data) {
+            console.log(data);
+            window.location.reload();
+            //alert("Producto borrado correctamente.");
+        },
+        error: function (e) {
+            console.log(e);
+            console.log("Error");
+
+        }
+
+
+    }); //fin de ajax
+} //fin function editar
+
+
+
+function Modal1(Id, Nombre, Descripcion, Precio, Foto) {
+    console.log(Nombre);
+/* 
+    $Foto = $fila['Foto'];
+    $Nombre = $fila['Nombre'];
+    $Descripcion = $fila['Descripcion'];
+    $Precio = $fila['Precio'];
+    $ID = $fila['Id_producto'];  */
+
+    $('#Nombre').val(Nombre);
+    $('#Descripcion').val(Descripcion);
+    $('#Precio').val(Precio);
+    $('#Foto').attr('src',"img/" + Foto); 
+
+    $('#myModal').modal('show');
+           
+} //fin function 
+
+    function editar(id){
+        //console.log(id);        
+        $.ajax({
+            type:'POST',
+            url:'php/editproducto.php',
+            dataType: "json",
+            data:{id:id},
+            success:function(data){
+                if(data.status == 'ok'){
+                    
+                }else{
+                   //$('.user-content').slideUp();
+                    alert("Error al editar producto.");
+                } 
+            }
+        }); //fin de ajax
+    } //fin function editar
+
+
+
+
+
+
+
+
+
 // $(document).ready(function(){
 //     $('#btnEdit').on('click',function(){
 //         var user_id = $('#user_id').val();
@@ -21,65 +89,3 @@
 //         });
 //     });
 // });
-function showenmodal(id) {
-    //console.log(id);        
-    $.ajax({
-        type: 'POST',
-        url: 'php/editproducto.php',
-        //dataType: "json",
-        data: { id: id },
-        success: function (data) {
-            if (data.status == 'ok') {
-                // $('#userName').text(data.result.name);
-                // $('#userEmail').text(data.result.email);
-                // $('#userPhone').text(data.result.phone);
-                // $('#userCreated').text(data.result.created);
-                // $('.user-content').slideDown();
-            } else {
-                //$('.user-content').slideUp();
-                alert("Error al editar producto.");
-            }
-        }
-    }); //fin de ajax
-} //fin function editar
-
-    function editar(id){
-        //console.log(id);        
-        $.ajax({
-            type:'POST',
-            url:'php/editproducto.php',
-            dataType: "json",
-            data:{id:id},
-            success:function(data){
-                if(data.status == 'ok'){
-                    
-                }else{
-                   //$('.user-content').slideUp();
-                    alert("Error al editar producto.");
-                } 
-            }
-        }); //fin de ajax
-    } //fin function editar
-
-    function borrar(Id) {
-        console.log(Id);
-        $.ajax({
-            type: 'POST',
-            url: 'php/deleteproducto.php',
-            //dataType: "json",
-            data: { id:Id },
-            success: function(data) {
-                console.log(data);
-                window.location.reload();
-                //alert("Producto borrado correctamente.");
-            },
-            error:function(e){
-                console.log(e);
-                console.log("Error");
-
-            }
-            
-            
-        }); //fin de ajax
-    } //fin function editar
-    
